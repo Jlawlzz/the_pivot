141e95e9b99b29daec8d728e6f68df9812edfbf2
Brant Wellman <BrantWellman@gmail.com>
2016-02-07 12:57:19 -0700

    Set up relationship between businesses and humans. Business can win a bid

--- spec/features/a_registered_admin_can_bid_and_acquire_a_human_spec.rb
+++ spec/features/a_registered_admin_can_bid_and_acquire_a_human_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "admin can bid and acquire human" do
   scenario "admin can bid on a human" do
-
     human = create(:human)
     auction = Auction.create(human_id: human.id)
     business = create(:business)
@@ -46,5 +45,40 @@ RSpec.feature "admin can bid and acquire human" do
     expect(page).to have_content "You have succesfully placed a bid of 1000!"
     expect(current_path).to eq auction_path(auction)
     expect(page).to have_content("Current Bid: $1000")
+  end
+
+  scenario "business wins bid and admin see human on business dashobaord" do
+    human = create(:human)
+    auction = Auction.create(human_id: human.id)
+    business = create(:business)
+
+    admin = User.create(first_name: "jordan",
+                        last_name: "guy",
+                        username: "admin",
+                        password: "password",
+                        role: 1)
+
+    business.users << admin
+
+    login(admin)
+
+    visit dashboard_path
+    click_link "Bid Now"
+    click_link "Bid Now!"
+    within("#bid") do
+      fill_in "Amount", with: "1000"
+      click_on "Bid Now"
+    end
+
+    click_on "End Auction"
+    visit business_path(business.url, business.id)
+    expect(page).to have_content(human.scum_name)
+
+    visit admin_business_path(business.url, business.id)
+    expect(page).to have_content(human.scum_name)
+
+    visit auctions_path
+    expect(page).to_not have_content(human.scum_name)
+
   end
 end
