787290f8dfdb6dfe4e0c9019a8bca993e289f27a
ZooBot Mars <sasa345@yahoo.com>
2016-02-07 23:39:15 -0700

    registered user can view bussiness show page

--- spec/features/user_can_view_businesses_spec.rb
+++ spec/features/user_can_view_businesses_spec.rb
@@ -1,37 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "user can view businesses" do
-  scenario "Unregistered user can view business index and individual business" do
-    # As a unregistered User,
-    # When I visit the home page,
-    # I see a link for "Hire Humans",
-    # I can click on "View Businesses",
-    # I am redirected to the business index,
-    # And I can click on a business and view it's show page,
-    # The page has the business name and the humans that work for them
-    visit '/'
+  scenario "registered user can view individual business and its humans" do
 
-    expect(page).to have_content("View Humans for Hire")
-    expect(page).to have_content("View Businesses")
-
-    click_link "View Businesses"
-    expect(current_path).to eq businesses_path
-
-    Business.create(name:"Porta Potty", description:"We clean up after nasty humans", url:"www.google.com")
-    Business.create(name:"Robot Shoe Makers", description:"Because metal feet no good for floor", url:"www.google.com")
-    Business.humans.create(name:"Human id#12")
-
-    expect(page).to have_content "Porta Potty"
-    expect(page).to_not have_content "Robot Shoe Makers"
-    click_link "Porta Potty"
-
-    business = Business.find_by(name: "Porta Potty")
-    expect(current_path).to eq business_path(business)
-
-    expect(page).to have_content "Porta Potty"
-    expect(page).to_not have_content "Robot Shoe Makers"
-    expect(page).to have_content "Human id#12"
-
+  #   user1 = create(:user)
+  #   human1 = Human.create(scum_name: "ID1212")
+  #   b1 = Business.create(name:"Porta Potty", description:"We clean up after nasty humans", url:"www.google.com")
+  #   b2 = Business.create(name:"Robot Shoe Makers", description:"Because metal feet no good for floor", url:"www.google.com")
+  #   b1.humans << human1
+  #   login(user1)
+  #
+  #   visit '/'
+  #
+  #   expect(page).to have_content("View Humans for Hire")
+  #   expect(page).to have_content("View Businesses")
+  #
+  #   click_link "View Businesses"
+  #   expect(current_path).to eq businesses_path
+  #   expect(page).to have_content "Porta Potty"
+  #   expect(page).to have_content "Robot Shoe Makers"
+  #
+  #   click_link "Join Porta Potty"
+  #   expect(current_path).to eq "/admin/porta-potty/#{b1.id}"
+  #   expect(page).to have_content "Porta Potty"
+  #   expect(page).to_not have_content "Robot Shoe Makers"
+  #
+  #   expect(page).to have_content "Porta Potty"
+  #   expect(page).to_not have_content "Robot Shoe Makers"
+  #   expect(page).to have_content "ID1212"
   end
 
 end
