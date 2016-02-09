c1e4bd31e8bd57eeea518ff93fa2cced65db412f
ZooBot Mars <sasa345@yahoo.com>
2016-02-09 13:57:25 -0700

    registered user has option to bid as self

---
+++ spec/features/registered_user_can_bid_as_user.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "registered user has option to bid as user" do
+  scenario "bid option is available" do
+
+    user1 = create(:user)
+    b1 = Business.create(name:"Porta Potty", description:"We clean up after nasty humans", url:"www.google.com")
+    user1.businesses << b1
+    login(user1)
+
+    visit dashboard_path
+
+    within "#user#{user1.id}" do
+      expect(page).to have_content("Bid As Yourself")
+    end
+
+  end
+
+end
