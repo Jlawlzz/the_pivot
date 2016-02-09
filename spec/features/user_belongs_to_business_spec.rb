a1ebbfea88a84d52d71161e7119ccd3fbfd4fff1
ZooBot Mars <sasa345@yahoo.com>
2016-02-08 17:41:24 -0700

    current business method implemented

---
+++ spec/features/user_belongs_to_business_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.feature "user can create account from home" do
+  scenario "registered has current business updated if they go from one to another" do
+
+    user = User.create(first_name:"charissa", last_name:"lawrence", username:"claw", password:"password", password_confirmation:"password")
+    business1 = Business.create(name:"Robot World")
+    business2 = Business.create(name:"Machine USA")
+    login(user)
+
+    user.businesses << business1
+    user.businesses << business2
+
+    visit dashboard_path
+
+    expect(page).to have_content "Robot World"
+    expect(page).to have_content "Machine USA"
+
+    first(:link, "Bid Now").click
+    expect(current_path).to eq auctions_path
+    expect(page).to have_content "Currently Bidding For: #{business1.name}"
+    expect(page).to_not have_content "#{business2.name}"
+
+  end
+
+  # scenario "is redirected to account page if username already exists" do
+  #   user = create(:user)
+  #
+  #   visit new_user_path
+  #
+  #   fill_in "First Name:", with: "Justin"
+  #   fill_in "Last Name:", with: "Lawlerz"
+  #   fill_in "Username:", with: user.username
+  #   fill_in "Password:", with: "password1"
+  #   fill_in "Password Confirmation", with: "password1"
+  #
+  #   within "form#new_user" do
+  #     click_button "Create"
+  #   end
+  #
+  #   expect(current_path).to eq new_user_path
+  #   expect(page).to have_content "Username #{user.username} is already taken. Please choose a different username"
+  # end
+end
