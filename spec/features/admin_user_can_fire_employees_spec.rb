351f1e1c9bd6dd568122d166136d9b79983a11ea
Brant Wellman <BrantWellman@gmail.com>
2016-02-08 17:47:32 -0700

    Modify test

--- spec/features/admin_user_can_fire_employees_spec.rb
+++ spec/features/admin_user_can_fire_employees_spec.rb
@@ -4,20 +4,22 @@ RSpec.feature "admin user can fire employees" do
   scenario "admin can fire a human and human appears back on the auction page" do
 
     business = create(:business_with_humans)
-    business_admin = User.create(first_name: "Admin",
+    admin = User.create(first_name: "Admin",
                         last_name: "guy",
                         username: "admin",
                         password: "password",
                         role: 1)
 
-    business.users << business_admin
+    business.users << admin
+    login(admin)
+
     humans = [Human.first, Human.last]
 
-    visit business_path(business)
+    visit business_path(business, business.id)
 
     expect(page).to have_content("#{Human.first.name}")
     expect(page).to have_content("#{Human.last.name}")
 
-    
+    click_on "#{human.first.name}"
   end
 end
