2eca70999b124e91f7f684ca7e19a39ddd269ec7
Jordan Lawler <jordan.lawler@colorado.edu>
2016-02-11 12:37:02 -0700

    fixed view errors

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -1,14 +1,19 @@
 class HumansController < ApplicationController
 
   def decomission_human
-    business = Business.find(session[:business_id])
+    @business = Business.find(session[:business_id])
     @human = Human.find(params[:format])
     fired_human
     set_new_auction
-    redirect_to admin_business_path(business.url, business.id)
+    route_after_fire
   end
 
   private
+
+    def route_after_fire
+      redirect_to admin_business_path(@business.url, @business.id) if @business
+      redirect_to dashboard_path if @business == nil
+    end
 
     def set_new_auction
       @human.auctions << Auction.create
