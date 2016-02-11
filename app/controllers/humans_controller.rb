fce235187c701f8ebb2009be18166f262c463ff9
Brant Wellman <BrantWellman@gmail.com>
2016-02-11 11:14:23 -0700

    Final controller refactor

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -3,7 +3,7 @@ class HumansController < ApplicationController
   def decomission_human
     business = Business.find(session[:business_id])
     @human = Human.find(params[:format])
-    @human.auctions.last.update_attribute(:status, 'fired')
+    fired_human
     set_new_auction
     redirect_to admin_business_path(business.url, business.id)
   end
@@ -12,5 +12,9 @@ class HumansController < ApplicationController
 
     def set_new_auction
       @human.auctions << Auction.create
+    end
+
+    def fired_human
+      @human.auctions.last.update_attribute(:status, 'fired')
     end
 end
